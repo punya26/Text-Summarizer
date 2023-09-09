abe42914281bda9c84fd0f72b47b53ec22ddd3c1
sidesshmore <mosiddhesh@gmail.com>
2023-09-09 21:43:50 +0530

    errors resolved

--- textsummarizer/settings.py
+++ textsummarizer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t8m0z!byt5=df=17(71$ha%zvsi_y@bx0p9lg46t$tld!b-c8$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
